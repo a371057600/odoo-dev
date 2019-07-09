8eba57784f35d6b21f9e3afcd043a4fb8aabbbd5
1576316115 <wbw157@gmail.com>
2019-07-09 17:13:19 +0800

    update custom-addons

---
+++ custom-addons/library_member/__manifest__.py
@@ -0,0 +1,8 @@
+# __manifest__.py
+{
+    'name': 'Library Members',
+    'description': 'Manage people who will be able to borrow books.',
+    'author': 'Alan Hou',
+    'depends': ['library_app'],
+    'application': False,
+}
