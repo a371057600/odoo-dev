bc94cfe298a675b2efad181839515db7a5263c85
wanbowen <1576316115@qq.com>
2019-07-10 13:41:15 +0800

    Update library_book.py

--- custom-addons/library_member/models/library_book.py
+++ custom-addons/library_member/models/library_book.py
@@ -3,6 +3,8 @@ from odoo import fields, models
 class Book(models.Model):
 
     _inherit = 'library.book' # 使用_inherit类属性来声明所继承模型。
+    isbn = fields.Char(help="Use a valid ISBN-13 or ISBN-10.")
+    publisher_id = fields.Many2one(index=True)
     """
         _name是模型标识符，如果修改会发生什么呢？
         其实你可以修改，这时它会创建所继承模型的拷贝，成为一个新模型。
